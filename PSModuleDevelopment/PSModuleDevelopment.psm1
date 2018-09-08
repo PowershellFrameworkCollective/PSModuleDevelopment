1db8a3e6911a6b9d90690d619816fb3b5618f68c
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-09-08 09:49:07 +0200

    update version

--- PSModuleDevelopment/PSModuleDevelopment.psm1
+++ PSModuleDevelopment/PSModuleDevelopment.psm1
@@ -1,5 +1,5 @@
 ﻿$script:PSModuleRoot = $PSScriptRoot
-$script:PSModuleVersion = "2.2.5.26"
+$script:PSModuleVersion = "2.2.5.27"
 
 $script:doDotSource = $false
 if (Get-PSFConfigValue -FullName PSModuleDevelopment.Import.DoDotSource) { $script:doDotSource = $true }
