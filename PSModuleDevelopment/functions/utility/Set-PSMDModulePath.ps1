1a3a96463fb00b7af6da2a95793fe5ae67bcaf82
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2018-10-20 14:05:43 +0800

    Update Set-PSModulePath

--- PSModuleDevelopment/functions/utility/Set-PSMDModulePath.ps1
+++ PSModuleDevelopment/functions/utility/Set-PSMDModulePath.ps1
@@ -1,56 +1,80 @@
 ﻿function Set-PSMDModulePath
 {
-	<#
-		.SYNOPSIS
-			Sets the path of the module currently being developed.
-		
-		.DESCRIPTION
-			Sets the path of the module currently being developed.
-			This is used by several utility commands in order to not require any path input.
+<#
+	.SYNOPSIS
+		Sets the path of the module currently being developed.
 	
-			This is a wrapper around the psframework configuration system, the same action can be taken by running this command:
-			Set-PSFConfig -Module PSModuleDevelopment -Name "Module.Path" -Value $Path
+	.DESCRIPTION
+		Sets the path of the module currently being developed.
+		This is used by several utility commands in order to not require any path input.
 		
-		.PARAMETER Path
-			The path to set as currently developed module.
+		This is a wrapper around the psframework configuration system, the same action can be taken by running this command:
+		Set-PSFConfig -Module PSModuleDevelopment -Name "Module.Path" -Value $Path
+	
+	.PARAMETER Module
+		The module, the path of which to register.
+	
+	.PARAMETER Path
+		The path to set as currently developed module.
+	
+	.PARAMETER Register
+		Register the specified path, to have it persist across sessions
+	
+	.PARAMETER EnableException
+		Replaces user friendly yellow warnings with bloody red exceptions of doom!
+		Use this if you want the function to throw terminating errors you want to catch.
+	
+	.EXAMPLE
+		Set-PSMDModulePath -Path "C:\github\dbatools"
 		
-		.PARAMETER EnableException
-            Replaces user friendly yellow warnings with bloody red exceptions of doom!
-            Use this if you want the function to throw terminating errors you want to catch.
+		Sets the current module path to "C:\github\dbatools"
+	
+	.EXAMPLE
+		Set-PSMDModulePath -Path "C:\github\dbatools" -Register
 		
-		.EXAMPLE
-			Set-PSMDModulePath -Path "C:\github\dbatools"
-	
-			Sets the current module path to "C:\github\dbatools"
-	
-		.EXAMPLE
-			Set-PSMDModulePath -Path "C:\github\dbatools"
-			Register-PSFConfig -FullName 'PSModuleDevelopment.Module.Path'
-	
-			Sets the current module path to "C:\github\dbatools"
-			Then stores the setting in registry, causing it to be persisted acros multiple sessions.
-	#>
+		Sets the current module path to "C:\github\dbatools"
+		Then stores the setting in registry, causing it to be persisted acros multiple sessions.
+#>
 	[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions", "")]
 	[CmdletBinding()]
-	Param (
-		[Parameter(Mandatory = $true)]
+	param (
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'Module')]
+		[System.Management.Automation.PSModuleInfo]
+		$Module,
+		
+		[Parameter(Mandatory = $true, ParameterSetName = 'Path')]
 		[string]
 		$Path,
+		
+		[switch]
+		$Register,
 		
 		[switch]
 		$EnableException
 	)
 	
-	$resolvedPath = Resolve-Path -Path $Path
-	if (Test-Path -Path $resolvedPath)
+	process
 	{
-		if ((Get-Item $resolvedPath).PSIsContainer)
+		if ($Path)
 		{
-			Set-PSFConfig -Module PSModuleDevelopment -Name "Module.Path" -Value $resolvedPath
+			$resolvedPath = Resolve-PSFPath -Path $Path -Provider FileSystem -SingleItem
+			if (Test-Path -Path $resolvedPath)
+			{
+				if ((Get-Item $resolvedPath).PSIsContainer)
+				{
+					Set-PSFConfig -Module PSModuleDevelopment -Name "Module.Path" -Value $resolvedPath
+					if ($Register) { Register-PSFConfig -Module 'PSModuleDevelopment' -Name 'Module.Path' }
+					return
+				}
+			}
+			
+			Stop-PSFFunction -Target $Path -Message "Could not validate/resolve path: $Path" -EnableException $EnableException -Category InvalidArgument
 			return
 		}
+		else
+		{
+			Set-PSFConfig -Module PSModuleDevelopment -Name "Module.Path" -Value $Module.ModuleBase
+			if ($Register) { Register-PSFConfig -Module 'PSModuleDevelopment' -Name 'Module.Path' }
+		}
 	}
-	
-	Stop-PSFFunction -Target $Path -Message "Could not validate/resolve path: $Path" -EnableException $EnableException -Category InvalidArgument
-	return
 }
