d690630c7e6bb7dd10f7c2ca10a85b8f66382cd1
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2023-06-17 11:01:58 +0200

    Add integration test template

--- templates/AppLockerProject/build/prerequisites.ps1
+++ templates/AppLockerProject/build/prerequisites.ps1
@@ -1,19 +1,28 @@
 param
 (
     [string]
-    $DependencyPath = (Resolve-Path "$PSScriptRoot\requiredModules.psd1").Path
+    $DependencyPath = (Resolve-Path "$PSScriptRoot\requiredModules.psd1").Path,
+
+    [switch]
+    $BuildWorker
 )
 
 $psdependConfig = Import-PowerShellDataFile -Path $DependencyPath
 
-$null = Get-PackageProvider -Name NuGet -ForceBootstrap
+if ($BuildWorker.IsPresent)
+{
+    $null = Get-PackageProvider -Name NuGet -ForceBootstrap
 
-Save-Module -Name PackageManagement, PowerShellGet, PSDepend -Repository $psdependConfig.PSDependOptions.Parameters.Repository -Path $psdependConfig.PSDependOptions.Target -Force
+    Install-Module -Force -Name PackageManagement, PowerShellGet -Repository $psdependConfig.PSDependOptions.Parameters.Repository -Scope CurrentUser
 
-Remove-Module -Name PowerShellGet -ErrorAction SilentlyContinue -Force
-Remove-Module -Name PackageManagement -ErrorAction SilentlyContinue -Force
-Import-Module -Force -Name (Join-Path -Path $psdependConfig.PSDependOptions.Target -ChildPath PackageManagement\*\PackageManagement.psd1 -Resolve)
-Import-Module -Force -Name (Join-Path -Path $psdependConfig.PSDependOptions.Target -ChildPath PowerShellGet\*\PowerShellGet.psd1 -Resolve)
+    Remove-Module -Name PowerShellGet -ErrorAction SilentlyContinue -Force
+    Remove-Module -Name PackageManagement -ErrorAction SilentlyContinue -Force
+    Import-Module -Force -Name PowerShellGet
+    Import-Module -Force -Name PackageManagement
+
+    $null = Install-WindowsFeature -Name GPMC
+}
+
+Save-Module -Name PSDepend -Repository $psdependConfig.PSDependOptions.Parameters.Repository -Path $psdependConfig.PSDependOptions.Target -Force
 Import-Module -Name (Join-Path -Path $psdependConfig.PSDependOptions.Target -ChildPath PSDepend\*\PSDepend.psd1 -Resolve)
-
 Invoke-PSDepend -Path $DependencyPath -Force
--- templates/AppLockerProject/build/requiredModules.psd1
+++ templates/AppLockerProject/build/requiredModules.psd1
@@ -14,5 +14,5 @@
     'Sampler.DscPipeline' = '0.2.0-preview0015' # Unfortunately still in preview
     Datum                 = '0.40.1'
     'Datum.InvokeCommand' = '0.3.0'
-    AppLockerFoundry      = '1.1.0'
+    AppLockerFoundry      = '1.1.5'
 }
