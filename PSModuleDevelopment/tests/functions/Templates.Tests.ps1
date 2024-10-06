0e83742bbab01d063726688c249d0c4fe6313000
FriedrichWeinmann <FriedrichWeinmann@users.noreply.github.com>
2024-10-06 13:20:30 +0200

    pathing issue

--- PSModuleDevelopment/tests/functions/Templates.Tests.ps1
+++ PSModuleDevelopment/tests/functions/Templates.Tests.ps1
@@ -1,12 +1,7 @@
 ﻿Describe "Verifying templating component" {
 	BeforeAll {
-		$tmpFolder = New-PSFTempDirectory -Name Template -ModuleName PSModuleDevelopment
 		$resourcePath = Resolve-PSFPath -Path "$PSScriptRoot\..\resources"
 		$templateName = 'TestTemplate-{0}' -f (Get-Random)
-	}
-	AfterAll {
-		Write-Host "Path: $resourcePath"
-		Remove-PSFTempItem -Name Template -ModuleName PSModuleDevelopment
 	}
 
 	It "Should Record the template correctly" {
@@ -21,8 +16,8 @@
 	}
 
 	It "Should Invoke the template correctly" {
-		{ Invoke-PSMDTemplate -TemplateName $templateName -OutPath $tmpFolder -Name Test -EnableException } | Should -Not -Throw
-		$content = Get-Content -Path "$tmpFolder\Test.txt" -ErrorAction Stop
+		{ Invoke-PSMDTemplate -TemplateName $templateName -OutPath TestDrive:\ -Name Test -EnableException } | Should -Not -Throw
+		$content = Get-Content -Path "TestDrive:\Test.txt" -ErrorAction Stop
 		$values = $content | ConvertFrom-StringData -ErrorAction Stop
 		$values.Name | Should -Be Test
 		$values.Value | Should -Be '123'
