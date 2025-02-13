4d5d764fbca16cc062229b579c0d747a897fd8c7
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2025-02-13 07:59:32 +0100

    Add event grid runner

---
+++ templates/AzureFunctionEventGrid/run.ps1
@@ -0,0 +1,23 @@
+﻿param (
+	$EventGridEvent,
+	
+	$TriggerMetadata
+)
+
+Write-Host "Trigger: %COMMAND% has been invoked"
+
+$parameters = Get-RestParameter -Request $EventGridEvent -Command %COMMAND%
+
+try {
+	$results = %COMMAND% @parameters -ErrorAction Stop
+}
+catch {
+	$_ | Out-String | ForEach-Object {
+		foreach ($line in ($_ -split "`n")) {
+			Write-Warning $line
+		}
+	}
+	Write-FunctionResult -Status InternalServerError -Body "$_"
+	return
+}
+Write-FunctionResult -Status OK -Body $results
